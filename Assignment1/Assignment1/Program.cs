d17d5aa9d3915bf4c400d587f708979cfddf7ad1
penceajr <penceajr@yahoo.com>
2020-12-07 13:48:41 +0200

    first assignment

---
+++ Assignment1/Assignment1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string allYourNumbers = ReadNumbersFromKeyboard();
+           string yourSeparator = ReadSeparatorFromKeyboard();
+           string resultedString="";
+            int counter = 0;
+           foreach(string userNumber in allYourNumbers.Split(" "))
+            {
+                if (counter < allYourNumbers.Split(" ").Count() - 1) { 
+                
+                    resultedString = resultedString + userNumber + yourSeparator;
+                
+                    counter = counter + 1;
+                }
+                else
+                    resultedString = resultedString + userNumber;
+            }
+
+            Console.Write(resultedString);
+        }
+
+        static string ReadNumbersFromKeyboard()
+        {
+
+            Console.WriteLine("Type 10 numbers: ");
+
+            string input = Console.ReadLine();
+            if (input.Split(" " ).Count() > 1 || String.IsNullOrEmpty(input))
+                return input;
+            else
+            {
+                Console.WriteLine("Please insert at least 2 numbers");
+                return ReadNumbersFromKeyboard();
+            }
+        }
+
+        static string ReadSeparatorFromKeyboard()
+        {
+
+            Console.WriteLine("Type Your Separator: ");
+            
+            string separator= Console.ReadLine();
+
+            if (!String.IsNullOrEmpty(separator))
+                return separator;
+            else
+            {
+                Console.WriteLine("Please insert at least 1 separator");
+                return ReadSeparatorFromKeyboard();
+            }
+
+        }
+    }
+}
