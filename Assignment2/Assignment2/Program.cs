dd4ebba13fc6e6feee9ada850e0c0458f30b0f9c
penceajr <penceajr@yahoo.com>
2020-12-07 14:47:58 +0200

    Assignment3

---
+++ Assignment2/Assignment2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+
+        static string ReadFirstStringFromKeyboard()
+        {
+
+            Console.WriteLine("Type 10 numbers: ");
+
+            string input = Console.ReadLine();
+            if (input.Split(" ").Count() > 1 || String.IsNullOrEmpty(input))
+                return input;
+            else
+            {
+                Console.WriteLine("Please insert at least 2 numbers");
+                return ReadNumbersFromKeyboard();
+            }
+        }
+    }
+}
---
+++ Assignment3/Assignment3/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Globalization;
+using System.Threading;
+
+namespace Assignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstString = ReadFirstStringFromKeyboard();
+            string secondString = ReadSecondStringFromKeyboard();
+            int counter = 0;
+            
+            foreach (string word in firstString.Split(" "))
+            {
+
+                if (secondString.Equals(word, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    counter = counter + 1;
+                }
+            }
+
+            Console.Write($"Your word is found {counter} time in your first string!");
+        }
+
+
+
+        static string ReadFirstStringFromKeyboard()
+        {
+
+            Console.WriteLine("Type your string: ");
+
+            string readFirstString = Console.ReadLine();
+            if (!String.IsNullOrEmpty(readFirstString))
+                return readFirstString;
+            else
+            {
+                Console.WriteLine("Please insert at least 1 word");
+                return ReadFirstStringFromKeyboard();
+            }
+        }
+        static string ReadSecondStringFromKeyboard()
+        {
+
+            Console.WriteLine("Type your string: ");
+
+            string readSecondString = Console.ReadLine();
+            if (!String.IsNullOrEmpty(readSecondString))
+                return readSecondString;
+            else
+            {
+                Console.WriteLine("Please insert at least 1 word");
+                return ReadSecondStringFromKeyboard();
+            }
+        }
+    }
+}
