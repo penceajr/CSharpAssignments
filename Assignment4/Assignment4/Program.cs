4000ab903210bd729a3d7f945ab171f95e30f46d
penceajr <penceajr@yahoo.com>
2020-12-07 15:12:36 +0200

    Assignment 4 doane

---
+++ Assignment4/Assignment4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Assignment4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Please enter your word: ");
+            string yourString = Console.ReadLine();
+
+            if (getStatus(yourString))
+            {
+                Console.WriteLine($"{yourString} is a palindrome word!!");
+            }
+            else
+            {
+                Console.WriteLine($"{yourString} is not a palindrome word!!");
+            }
+        }
+
+
+        public static bool getStatus(string myString)
+        {
+            string first = myString.Substring(0, myString.Length / 2);
+            char[] arr = myString.ToCharArray();
+            Array.Reverse(arr);
+            string temp = new string(arr);
+            string second = temp.Substring(0, temp.Length / 2);
+            return first.Equals(second);
+        }
+    }
+}
